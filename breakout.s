86d2e54e49f4a01f50e3b31e01d8f7b2d47f1d53
Ryan Sandor Richards <sandor.richards@gmail.com>
2012-07-17 02:53:50 -0700

    Added a logo and tested rendering.

--- breakout.s
+++ breakout.s
@@ -12,22 +12,107 @@
 reset:
 .include "include/reset.s"
 
+;;;;;;;;;;;;;; Macros ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.macro vram hi, lo
+	lda hi
+	sta $2006
+	lda lo
+	sta $2006
+.endmacro
+
 ;;;;;;;;;;;;;; Main Program ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main:
+
+
+load_palette:
+	ldx #$00
+	vram #$3f, #$00
+@loop:	lda palette, x
+	sta $2007
+	inx
+	cpx #$20
+	bne @loop
+
+set_attributes:
+	ldx #$40
+	vram #$23, #$c0
+	lda #%01010101
+@loop:	sta $2007
+	dex
+	bne @loop
+
+draw_logo:
+	
+	lo = $20
+	hi = $21
+
+	lda #$21
+	sta hi
+	lda #$88
+	sta lo
+
+	ldy #$00
+@row:	vram hi, lo
+	ldx #$10
+@col:	sty $2007
+	iny
+	dex
+	bne @col
+	
+	clc
+	lda lo
+	adc #$20
+	sta lo
+	lda #$00
+	adc hi
+	sta hi
+
+	cpy #$40
+	bne @row
+
+enable_rendering:
+	lda #%10000000	; Enable NMI
+	sta $2000
+	lda #%00011110	; Enable Sprites & Background
+	sta $2001
+	lda #$00 	; Reset the VRAM address
+	sta $2006
+	sta $2006
 
 forever:
 	jmp forever
 
 
-;;;;;;;;;;;;;; Game Loop ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-nmi:
+;;;;;;;;;;;;;; Game Loop (NMI) ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+game_loop:
 	rti
+
+;;;;;;;;;;;;;; Subroutines ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;; Palettes, Nametables, etc. ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+palette:
+	; Background
+	.byte $0f, $03, $00, $00
+	.byte $0f, $19, $00, $00
+	.byte $0f, $00, $00, $00
+	.byte $0f, $00, $00, $00
+
+	; Sprites
+	.byte $0f, $00, $00, $00
+	.byte $0f, $00, $00, $00
+	.byte $0f, $00, $00, $00
+	.byte $0f, $00, $00, $00
+
 
 ;;;;;;;;;;;;;; Pattern Table (CHR-ROM) ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .segment "CHARS"
+.include "include/logo.s"
 
 ;;;;;;;;;;;;;; Vectors ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .segment "VECTORS"
-.word 0, 0, 0, nmi, reset, 0
+.word 0, 0, 0, game_loop, reset, 0
